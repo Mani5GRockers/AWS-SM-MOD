497b244b315870ca89bebbf68301011107cbc838
Mani5GRockers <66015149+Mani5GRockers@users.noreply.github.com>
2021-09-17 03:36:48 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -56,7 +56,7 @@ Type /{BotCommands.HelpCommand} to get a list of available commands
         sendMarkup(start_string, context.bot, update, reply_markup)
     else:
         sendMarkup(
-            'Oops! not a Authorized user.\nPlease deploy your own <b>slam-mirrorbot</b>.',
+            'Oops! not a Authorized user.\nPlease Join <b>aws-mirrorzone-group</b>.',
             context.bot,
             update,
             reply_markup,
