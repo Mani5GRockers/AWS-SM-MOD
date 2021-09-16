b4c26fd17bdae64437a63b9a26b4cc2eea473ffe
Mani5GRockers <66015149+Mani5GRockers@users.noreply.github.com>
2021-09-17 03:38:40 +0530

    Update bot_commands.py

--- bot/helper/telegram_helper/bot_commands.py
+++ bot/helper/telegram_helper/bot_commands.py
@@ -7,7 +7,7 @@ class _BotCommands:
         self.ZipMirrorCommand = 'zipmirror'
         self.CancelMirror = 'cancel'
         self.CancelAllCommand = 'cancelall'
-        self.ListCommand = 'list'
+        self.ListCommand = 'search'
         self.StatusCommand = 'status'
         self.AuthorizedUsersCommand = 'users'
         self.AuthorizeCommand = 'authorize'
