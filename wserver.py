c67d8a34c7abc4479e2f7f890768f20ee851606d
Mani5GRockers <66015149+Mani5GRockers@users.noreply.github.com>
2021-09-19 23:45:20 +0530

    Update wserver.py

--- wserver.py
+++ wserver.py
@@ -25,8 +25,8 @@ page = """
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Torrent File Selector</title>
-    <link rel="icon" href="https://telegra.ph/file/6507910fd06d18dfaba82.jpg" type="image/jpg">
+    <title>File Selector</title>
+    <link rel="icon" href="https://gitlab.awslink.in/api/logoposter.jpg" type="image/jpg">
     <script
       src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
       integrity="sha256-4+XzXVhsDmqanXGHaHvgh1gMQKX40OUvDEBTu8JcmNs="
@@ -202,16 +202,16 @@ input[type="submit"]:hover, input[type="submit"]:focus{
     <header>
       <div class="brand">
         <img
-          src="https://telegra.ph/file/6507910fd06d18dfaba82.jpg"
+          src="https://gitlab.awslink.in/api/logoposter.jpg"
           alt="logo"
         />
-        <a href="https://t.me/SlamMirrorUpdates"> 
-          <h2 class="name">slam-mirrorbot</h2>
+        <a href="https://telegram.me/Mani5GRockers"> 
+          <h2 class="name">Mani5GRockers</h2>
         </a>
       </div>
       <div class="social">
-        <a href="https://github.com/SlamDevs/slam-mirrorbot"><i class="fab fa-github"></i></a>
-        <a href="https://t.me/SlamMirrorUpdates"><i class="fab fa-telegram"></i></a>
+        <a href="https://github.com/Mani5GRockers"><i class="fab fa-github"></i></a>
+        <a href="https://telegram.me/Mani5GRockers"><i class="fab fa-telegram"></i></a>
       </div>
     </header>
     <section>
@@ -330,8 +330,8 @@ code_page = """
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Torrent Code Checker</title>
-    <link rel="icon" href="https://telegra.ph/file/6507910fd06d18dfaba82.jpg" type="image/jpg"> 
+    <title>Code Checker</title>
+    <link rel="icon" href="https://gitlab.awslink.in/api/logoposter.jpg" type="image/jpg"> 
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link
@@ -527,16 +527,16 @@ section span{
     <header>
       <div class="brand">
         <img
-          src="https://telegra.ph/file/6507910fd06d18dfaba82.jpg"
+          src="https://gitlab.awslink.in/api/logoposter.jpg"
           alt="logo"
         />
-        <a href="https://t.me/SlamMirrorUpdates">
-          <h2 class="name">slam-mirrorbot</h2>
+        <a href="https://telegram.me/Mani5GRockers">
+          <h2 class="name">Mani5GRockers</h2>
         </a>
       </div>
       <div class="social">
-        <a href="https://github.com/SlamDevs/slam-mirrorbot"><i class="fab fa-github"></i></a>
-        <a href="https://t.me/SlamMirrorUpdates"><i class="fab fa-telegram"></i></a>
+        <a href="https://github.com/Mani5GRockers"><i class="fab fa-github"></i></a>
+        <a href="https://telegram.me/Mani5GRockers"><i class="fab fa-telegram"></i></a>
       </div>
     </header>
     <section>
@@ -705,7 +705,7 @@ async def set_priority(request):
 @routes.get('/')
 async def homepage(request):
 
-    return web.Response(text="<h1>See slam-mirrorbot <a href='https://github.com/SlamDevs/slam-mirrorbot'>@GitHub</a> By <a href='https://github.com/SlamDevs'>SlamDevs</a></h1>", content_type="text/html")
+    return web.Response(text="<h1>Mani5GRockers <a href='https://github.com/Mani5GRockers'>@GitHub</a> By <a href='https://github.com/Mani5GRockers'>Mani5GRockers</a></h1>", content_type="text/html")
 
 
 async def e404_middleware(app, handler):
@@ -715,11 +715,11 @@ async def e404_middleware(app, handler):
         try:
             response = await handler(request)
             if response.status == 404:
-                return web.Response(text="<h1>404: Page not found</h2><br><h3>slam-mirrorbot</h3>", content_type="text/html")
+                return web.Response(text="<h1>404: Page not found</h2><br><h3>Mani5GRockers</h3>", content_type="text/html")
             return response
         except web.HTTPException as ex:
             if ex.status == 404:
-                return web.Response(text="<h1>404: Page not found</h2><br><h3>slam-mirrorbot</h3>", content_type="text/html")
+                return web.Response(text="<h1>404: Page not found</h2><br><h3>Mani5GRockers</h3>", content_type="text/html")
             raise
     return middleware_handler
 
